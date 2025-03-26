7d8b5eaf9e8245d0ee90f6517c1cb3e7c469684d
xtt28 <155261788+xtt28@users.noreply.github.com>
2025-03-26 10:32:43 -0400

    Create and install custom user model.

--- ai_study/ai_study/settings.py
+++ ai_study/ai_study/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Custom settings added by this application.
+
+AUTH_USER_MODEL = 'users.CustomUser'
