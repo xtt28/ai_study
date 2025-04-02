80d704cec65a15a294b15b8a90e9c96ddaeac481
xtt28 <155261788+xtt28@users.noreply.github.com>
2025-04-02 09:12:45 -0400

    Create models in study_guide app.

--- ai_study/ai_study/settings.py
+++ ai_study/ai_study/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users',
+    'study_guides',
 ]
 
 MIDDLEWARE = [
@@ -127,3 +128,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Custom settings added by this application.
 
 AUTH_USER_MODEL = 'users.CustomUser'
+
+MEDIA_ROOT = 'media'
--- ai_study/study_guides/models.py
+++ ai_study/study_guides/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+class StudyGuide(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name="study_guides")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=255)
+    file = models.FileField(upload_to="source_files")
+
+    def __str__(self):
+        return f'Study guide "{self.title}" by {self.user.username}'
+    
+class TextStudyGuide(models.Model):
+    study_guide = models.OneToOneField(StudyGuide, on_delete=models.CASCADE, related_name="text_study_guide")
+    content = models.TextField()
