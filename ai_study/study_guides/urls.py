fd67634f7ebcc76bc33b972a1970f8eb9835d1ea
xtt28 <155261788+xtt28@users.noreply.github.com>
2025-04-23 09:45:35 -0400

    Begin study guide list view.

--- ai_study/study_guides/urls.py
+++ ai_study/study_guides/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("guide/new", views.StudyGuideCreateView.as_view(), name="create"),
-    path("guide/<int:pk>/", views.StudyGuideDetailView.as_view(), name="detail")
+    path("guides/new", views.StudyGuideCreateView.as_view(), name="create"),
+    path("guides/<int:pk>/", views.StudyGuideDetailView.as_view(), name="detail"),
+    path("guides/list", views.StudyGuideListView.as_view(), name="list")
 ]
 app_name = "study_guides"
--- ai_study/study_guides/views.py
+++ ai_study/study_guides/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.edit import CreateView
 from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
 from . import models
 
 class StudyGuideDetailView(DetailView):
@@ -14,3 +15,9 @@ class StudyGuideCreateView(CreateView):
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
+    
+
+class StudyGuideListView(ListView):
+    model = models.StudyGuide
+    paginate_by = 10
+    ordering = "-updated_at"
